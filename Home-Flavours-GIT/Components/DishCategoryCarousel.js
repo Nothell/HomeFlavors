229fa80d1b3474a42c348f208f329416db23e7ef
Noel Soans <noelsoans57@gmail.com>
2023-10-19 10:28:28 -0400

    HF-19_PLP_Design_Enchancement

--- Home-Flavours-GIT/Components/DishCategoryCarousel.js
+++ Home-Flavours-GIT/Components/DishCategoryCarousel.js
@@ -1,28 +1,25 @@
-
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import {  collection, getDocs, addDoc, query, where } from 'firebase/firestore';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image, TextInput } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
 import { useRoute } from '@react-navigation/native';
 import { db } from '../firebaseConfig';
 
-
 export default function DishCategoryCarousel() {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
     const route = useRoute();
     const { itemDta } = route.params;
-    // Sample product data
+
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const productCollectionRef = collection(db, 'products'); // Replace 'products' with your Firestore collection name
+                const productCollectionRef = collection(db, 'products');
                 const productSnapshot = await getDocs(productCollectionRef);
 
                 const productsData = [];
                 productSnapshot.forEach((doc) => {
                     productsData.push({ id: doc.id, ...doc.data() });
                 });
-                console.log("Hereeeeeeeeeeeeeeeeeeeeeee")
-                console.log(productsData)
                 setProducts(productsData);
             } catch (error) {
                 console.error('Error fetching products from Firestore: ', error);
@@ -31,6 +28,9 @@ export default function DishCategoryCarousel() {
 
         fetchProducts();
     }, []);
+
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+
     const renderItem = ({ item }) => (
         <TouchableOpacity style={styles.productTile}>
             <Image source={{ uri: item.image }} style={styles.productImage} />
@@ -40,23 +40,27 @@ export default function DishCategoryCarousel() {
 
     return (
         <View style={styles.container}>
+            <View style={styles.searchBarContainer}>
+                <TextInput
+                    style={styles.searchBar}
+                    placeholder="Search..."
+                    onChangeText={text => setSearch(text)}
+                />
+            </View>
             <Text style={styles.title}>{itemDta}</Text>
             <FlatList
-                data={products}
+                data={filteredProducts}
                 keyExtractor={(item) => item.id}
                 renderItem={renderItem}
-                numColumns={1} // You can adjust the number of columns as needed
-                columnWrapperStyle={styles.column}
+                numColumns={2} // Display products in 2 columns
             />
         </View>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        // alignItems: 'center',
         padding: 16,
         width: '100%',
         backgroundColor: 'white',
@@ -71,13 +75,13 @@ const styles = StyleSheet.create({
         margin: 8,
         borderRadius: 8,
         overflow: 'hidden',
-        backgroundColor: 'grey',
+        backgroundColor: 'lightgreen',
         elevation: 3, // for Android
         shadowColor: '#000',
-        shadowOffset: { width: 100, height: 2 },
+        shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.3,
         shadowRadius: 3,
-        width: '100%',
+        width: '45%', // Adjust the width as needed
     },
     productImage: {
         width: '100%',
@@ -88,8 +92,15 @@ const styles = StyleSheet.create({
         padding: 8,
         textAlign: 'center',
     },
-    row: {
-        // flexWrap: 'wrap',
-        width: '100%',
+    searchBarContainer: {
+        backgroundColor: 'lightgreen',
+        borderRadius: 20,
+        padding: 10,
+        margin: 10,
+    },
+    searchBar: {
+        flex: 1,
+        padding: 0,
+        fontSize: 16,
     },
 });
